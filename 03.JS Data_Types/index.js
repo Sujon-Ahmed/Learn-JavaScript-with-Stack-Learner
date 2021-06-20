a0e2bfa8d7b11c0ed644cc616f0b53ab61975ba5
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-20 15:44:28 +0600

    use parseInt and parseFloat

--- 03.JS Data_Types/index.js
+++ 03.JS Data_Types/index.js
@@ -2,5 +2,8 @@
 
 var n = 123;
 var f = 3.1416;
-var nn = Number('45');
+var nn = Number('45.45');
 console.log(nn);
+
+console.log(parseInt(nn)); // convert any type to int
+console.log(parseFloat(nn)); // convert any type to float
