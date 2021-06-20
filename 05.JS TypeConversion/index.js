68efa73efa159f2614053cb7683a3a6f546b0160
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-20 16:53:28 +0600

    falsy value

--- 05.JS TypeConversion/index.js
+++ 05.JS TypeConversion/index.js
@@ -4,3 +4,20 @@ console.log(str * num);
 
 console.log(Number.parseInt(str));
 console.log(num.toString());
+
+console.log(Boolean(Infinity));
+
+//This value isn't false but javaScript compiler always this value false
+
+// Falsy value ex:- '', 0, null, undefined, NaN etc.
+
+console.log(Boolean(''))
+console.log(Boolean('Hello'))
+console.log(Boolean(0))
+console.log(Boolean(786))
+console.log(Boolean(null))
+console.log(Boolean(undefined))
+
+console.log(true)
+var x = true
+console.log(x.toString())
