88a8f745d00532d8ad010e36988b2c8a97cf3c0b
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-06-20 15:58:49 +0600

    JS String

---
+++ 04.JS Strings/index.js
@@ -0,0 +1,11 @@
+//String
+
+var str = 'String';
+var str2 = "String";
+var str3 = `String`;
+
+var str4 = String('String');
+var str5 = String(156);
+var str6 = String(3.1416);
+
+console.log(str4, str5, str6);
