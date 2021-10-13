45c0221cdc1515fb37bb717b6596fc5561461f7e
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-10-13 11:35:11 +0600

    learn basic js if else condition

---
+++ chapter_three/02.js_else_condition/index.js
@@ -0,0 +1,17 @@
+var a = 10
+var b = 20
+
+if(a > b){
+    console.log("A is gether than B") 
+}else{
+    console.log("B is gether than A")
+}
+
+// even or odd number program
+var n = 5
+
+if(n%2 == 0){
+    console.log(n + " is Even Number")
+}else{
+    console.log(n + " is Odd Number")
+}
