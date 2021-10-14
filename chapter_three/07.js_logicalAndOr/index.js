ae47f811dd956034738330edf39c3cfc11fe6741
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-10-14 11:42:36 +0600

    learn logical AND OR operator shorthand

---
+++ chapter_three/07.js_logicalAndOr/index.js
@@ -0,0 +1,9 @@
+// shorthand used && , || operators
+// || operator shorthand
+var name = ''
+var fullName = name || 'Sujon Ahmed'
+console.log(fullName)
+
+// && operator shorthand
+var isOk = true
+isOk && console.log("Everything is Ok")
