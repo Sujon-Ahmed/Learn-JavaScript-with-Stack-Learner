f2c4161b0590ec4fcb80b316f4c4e1fdf1b7174d
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-10-14 12:20:09 +0600

    learn js for loop

---
+++ chapter_four/01.js_for_loop/index.js
@@ -0,0 +1,32 @@
+// for loop
+// for(var i=0; i<=10; i++){
+//     console.log(i)
+// }
+
+// find 1 to 100 odd number
+// for(var i = 1; i<=100; i++){
+//     if(i%2 == 1){
+//         console.log(i)
+//     }
+// }
+
+// find 1 to 100 even number
+// for(var i = 1; i<=100; i++){
+//     if(i%2 == 0){
+//         console.log(i)
+//     }
+// }
+
+// 1 to 50 number summation
+// var sum = 0;
+// for(var i = 1; i<=10; i++){
+//     console.log(sum + ' + ' + i + ' = ' + (sum+i))
+//     sum+=i
+// }
+
+// H.W Done
+var sum = 2;
+for(var i = 2; i<=100; i+=2){
+    console.log(sum + ' + ' + i + ' = ' + (sum+i))
+    sum+=i
+}
