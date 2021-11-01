c4c217a7d3cfa88a36bf6f4cbdbcc13c4517670c
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-11-01 15:54:01 +0600

    string comparison

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -6,10 +6,19 @@ var str = String(n); // String Constructor
 var str1 = new String(n); // String Object
 // console.log(str);
 
-// **** Escape Notation
+// **** Escape Notation ****//
 var str = 'This is a \'String\'';   //? use single quote
 var str1 = 'This is a \"String\"';   //? use double quote
 var str2 = 'This is a \\ String';   //? use Backslash
 var str3 = 'This is a \tString';   //? use Tab
 var str4 = 'This is a \nString';   //? use new line
 // console.log(str4);
+
+// **** String Comparison ****//
+// TODO -> JavaScript Uses Lexicographic System to Compare to String
+// ? Always Remember that lowercase z-a is gether than Z-A
+var a = 'a'; 
+var b = 'Z';
+//console.log(a > b); // return true
+console.log('001' == 1); // return true 
+// ? JavaScript Compiler always convert compare number and string both are number
