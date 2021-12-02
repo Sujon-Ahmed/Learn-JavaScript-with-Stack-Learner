9fd496ec1890b66a228f6256669f8828564b432d
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-02 11:14:08 +0600

    add variables

---
+++ chapter_two/variables.js
@@ -0,0 +1,13 @@
+// variables is a container which can store data
+
+// **! variable always declare fast
+var name = 'Sujon Ahmed';
+var age = 22;
+
+// console.log(name);
+// console.log(age);
+
+console.log(name + ' Knows JavaScript.');
+console.log('His age is only ' + age);
+console.log(name + ' is creating a JavaScript course for all');
+console.log('But his age is only ' + age);
