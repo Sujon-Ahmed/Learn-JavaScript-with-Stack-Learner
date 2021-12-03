30ac57d2b60abb4f9a2942c0e7b5918a79f31ccf
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-03 11:28:24 +0600

    learn comments

--- chapter_two/variables.js
+++ chapter_two/variables.js
@@ -11,3 +11,14 @@ console.log(name + ' Knows JavaScript.');
 console.log('His age is only ' + age);
 console.log(name + ' is creating a JavaScript course for all');
 console.log('But his age is only ' + age);
+
+var mathNumber = 80;
+var accountNumberDetails = 104;     // we are use this type variables
+var account_number_details = 104;
+
+// this is single line comment
+
+/*
+    this is multiline
+    comments
+*/
