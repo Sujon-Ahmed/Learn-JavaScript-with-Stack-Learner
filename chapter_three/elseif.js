f174d03794060d78478227d92ffab60cfde27bdf
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-03 18:21:36 +0600

    learn JS elseif condition

---
+++ chapter_three/elseif.js
@@ -0,0 +1,19 @@
+var a = 20;
+var b = 10;
+
+if(a > b) {
+    console.log('A is gather than B');
+} else if(a < b){
+    console.log('B is gather than A');
+} else{
+    console.log('They Both are Same');
+}
+
+var n = 15;
+if(n == 0){
+    console.log(n + ' is Zero');
+}else if(n % 2 == 0) {
+    console.log(n + ' is Even Number');
+} else{
+    console.log(n + ' id Odd Number');
+}
