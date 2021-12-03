f973d7dee2e7163529a1cb3b308735931d5a66ba
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-03 10:46:47 +0600

    learn JS Operators

---
+++ chapter_two/operator.js
@@ -0,0 +1,73 @@
+// JS Operators
+// *********** Arithmetic Operator **************
+// + - * / %
+
+// var a = 10;
+// var b = 3;
+// console.log(a % b);
+
+// Incremental
+// Pre Incremental - Post Incremental
+// ++a           -  a++
+
+// console.log(++a);
+// console.log(a++);
+// console.log(a);
+
+// Decremental
+// Pre Decremental - Post Decremental
+// --a          -   a--
+
+// console.log(--a);
+// console.log(a--);
+// console.log(a);
+
+// ********** Assignment Operator *************
+
+// var a = 10;
+// var b = 20;
+// a+=b;
+// console.log(a);
+
+// a-=b;
+// console.log(a);
+
+// a*=b;
+// console.log(a);
+
+
+// ********** Comparison Operator **************
+var a = 10;
+var b = 20;
+
+console.log(a == b);    // false
+console.log(a != b);    // true
+
+console.log(a > b);    // false
+console.log(a < b);    // true
+
+// var a = 20;
+
+console.log(a >= b);    // false
+console.log(a <= b);    // true
+
+var c = '50';
+var d = 50;
+console.log(c == d);    // true
+console.log(c === d);   // false
+
+// ********** Logical Operator ***********
+// &&
+// ||
+// !
+
+// ********** Bitwise Operator *************
+// &
+// |
+// !
+// ~
+// ^
+// >>
+// <<
+
+console.log(typeof('Hello'));
