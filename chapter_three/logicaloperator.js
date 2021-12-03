dd216f8f064e150a87355342d3d0fd7be2350bdb
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-03 22:04:26 +0600

    learn JS logical operator

---
+++ chapter_three/logicaloperator.js
@@ -0,0 +1,22 @@
+var a = 10;
+var b = 20;
+var c = 50;
+var d = 40;
+
+// And (&&) Operator Example
+if (a > b && c > d) {
+    console.log('A is gather than B And C is gather than D');
+} else {
+    console.log('At least one condition is false!');
+}
+
+// OR (||) Operator Example
+if (a > b || c > d) {
+    console.log('A is gather than B And C is gather than D');
+} else {
+    console.log('At least one condition is false!');
+}
+
+// NOT (!) Operator Example
+ var check = !(a > b);
+ console.log(check);
