e20a736cd8ea28ae25b0c497e951f8ad15819970
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-04 22:31:11 +0600

    learn JS Escape Notation

---
+++ chapter_five/string.js
@@ -0,0 +1,15 @@
+// Chapter Five
+// String in JavaScript
+// What is String?
+// Type of String
+
+var str = 'Something'; // String Literal
+var str2 = String('Something'); // String Construct
+
+var n = 10;
+var result = String(n);
+// console.log(result);
+
+// ******** Escape Notation ***********************
+var text = 'This is a \nString'; // \' \" \\ \b \n \t \v \r \f
+console.log(text);
