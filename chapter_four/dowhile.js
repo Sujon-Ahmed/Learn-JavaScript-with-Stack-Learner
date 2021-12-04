f579f5baeffd2b81259e58a8f61151e57101c720
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-04 11:02:11 +0600

    learn basic do while loop

---
+++ chapter_four/dowhile.js
@@ -0,0 +1,11 @@
+// Do While Loop
+var isRunning = false;
+// **! If isRunning = true , Then while loop Infinity 
+while (isRunning) {
+    console.log('I am Running with while');
+}
+
+// ** Do While Loop
+do {
+    console.log('I am Running with do while');
+} while (isRunning);
