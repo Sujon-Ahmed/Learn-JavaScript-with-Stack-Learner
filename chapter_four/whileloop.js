70483aea852b45a63882b7af14e068d0f82a7dec
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-04 10:55:08 +0600

    learn while loop with real example

---
+++ chapter_four/whileloop.js
@@ -0,0 +1,27 @@
+// while loop structure
+/*
+var = initializer;
+while (condition) {
+    statement....;
+    Increment++;
+}
+*/
+
+// var text = 0;
+// while (text <= 10) {
+//     console.log(text + ' Hello World');
+//     text++;
+// }
+
+// *? Project 
+
+var isRunning = true;
+while (isRunning) {
+    var rand = Math.floor(Math.random() * 10 + 1);
+    if (rand == 9) {
+        console.log('Winner Winner Chicken Dinner');
+        isRunning = false;
+    } else {
+        console.log('You have got ' + rand);
+    }
+}
