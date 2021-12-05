104acac37250cd270722b90a1363704928bbf9cb
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-05 13:42:23 +0600

    learn basic array literal, constrictor

---
+++ chapter_six/array.js
@@ -0,0 +1,15 @@
+//TODO Chapter Six : Working With Array
+
+// ? ******* What is Array ************
+// * Array is a Data structure where we can organize multiple data under a single variable
+
+var arr = [1, 2, 3, 4, 5]; // array literal
+// console.log(arr);
+// console.log(arr[1]); // array index check
+// console.log(arr.length);
+arr[5] = 6; // array value insert
+arr[2] = 22 // array value over right
+// console.log(arr);
+
+var arr2 = Array(1, 2, 3); // array constrictor
+console.log(arr2);
