4f255c38ccd80953c85a638e4d8db8db72101177
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-05 12:53:34 +0600

    string length

--- chapter_five/string.js
+++ chapter_five/string.js
@@ -24,4 +24,57 @@ var text = 'This is a \nString'; // \' \" \\ \b \n \t \v \r \f
 //console.log(a < b); // true
 
 // console.log('a' > 'Z');
-console.log('001' == 1);
+// console.log('001' == 1);
+
+// ********** String with build in method ***********
+
+var a = 'I am';
+var b = 'Sujon Ahmed';
+var c = 'Riman';
+var d = a.concat(' ',b,' ',c);
+// console.log(d);
+var e = d.substr(5,5);
+// console.log(e);
+
+// console.log(d.charAt(0));
+
+// console.log(d.startsWith('I am')); // true
+// console.log(d.startsWith('Sujon')); // false
+
+
+// console.log(d.endsWith('Riman')); // true
+// console.log(d.endsWith('Riman10')); // false
+
+// console.log(b.toLowerCase());
+// console.log(b.toUpperCase());
+
+// console.log('         Hello           '.trim());
+// console.log(d.split(' '));
+
+// ********** String Length *************
+// logical way
+var str = 'Sujon Ahmed';
+var length = 0;
+while (true) {
+    if (str.charAt(length) == '') {
+        break;
+    } else {
+        length++;
+    }
+}
+// console.log(length);
+
+// build in function way
+// console.log(str.length);
+console.log('Something'.length);
+
+
+
+
+
+
+
+
+
+
+
