163d1600c6ee4a621d65f4ef9cac60df52eb9ac6
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-06 22:39:51 +0600

    learn function

---
+++ chapter_eight/function.js
@@ -0,0 +1,35 @@
+// Chapter Eight : Functions in JavaScript
+
+// What is JavaScript
+
+// Function Declare Structure
+/*
+function functionName() {
+
+}
+*/
+
+function functionName() {
+    console.log('I am a Function');
+}
+
+function add() {
+    var a = 10;
+    var b = 20;
+    console.log(a + b);
+}
+
+
+function sub() {
+    var a = 50;
+    var b = 20;
+    console.log(a - b);
+}
+
+// functionName();
+// add();
+// sub();
+
+// for (var i = 0; i < 10; i++) {
+//     functionName();
+// }
