2a8fe5a42ab2af1576f25c675050eeff0d9b690a
Sujon Ahmed <sujonahmed5284@gmail.com>
2021-12-06 15:33:39 +0600

    array methods

---
+++ chapter_six/methods.js
@@ -0,0 +1,24 @@
+// Methods of Array
+
+// var arr = [1, 4, 5, 6, 7];
+// var arr2  = [5, 2, 9, 3, 8];
+
+// console.log(arr.join(' | '));
+
+// arr.fill(0);
+
+// var arr3 = arr.concat(arr2);
+// console.log(arr3);
+
+// console.log(Array.isArray(arr));
+
+// var arr2 = Array.from(arr);
+// var arr2 = arr;
+// console.log(arr2);
+
+
+var a = [1, 2];
+var b = Array.from(a);
+b[0] = 5;
+console.log(a);
+console.log(b);
